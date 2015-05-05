c14cd17d11f871366783d1f8ad9ccaddeedda082
lpm11 <lpm11r@gmail.com>
2015-05-05 21:50:36 +0900

    Added random image feature.

--- lib/lita/handlers/tiqav.rb
+++ lib/lita/handlers/tiqav.rb
@@ -4,6 +4,7 @@ module Lita
   module Handlers
     class Tiqav < Handler
       route /^tiqav (.*)$/i, :tiqav, command: false
+      route /^tiqav$/i, :tiqav_random, command: false
 
       def tiqav(response)
         query = response.matches[0][0]
@@ -15,6 +16,19 @@ module Lita
           url = results.first.url.to_s()
         rescue
           response.reply("#{query} not matched.")
+          return
+        end
+        response.reply(url)
+      end
+
+      def tiqav_random(response)
+        begin
+          image = ::Tiqav.random()
+          raise if (image.nil?)
+
+          url = image.url.to_s()
+        rescue
+          response.reply("Service unavailable.")
           return
         end
         response.reply(url)
--- spec/lita/handlers/tiqav_spec.rb
+++ spec/lita/handlers/tiqav_spec.rb
@@ -1,12 +1,21 @@
 require "spec_helper"
 
 describe Lita::Handlers::Tiqav, lita_handler: true do
+  it { is_expected.to route_command("tiqav").to(:tiqav_random) }
   it { is_expected.to route_command("tiqav ちくわ").to(:tiqav) }
 
   describe "search" do
     it "returns chikuwa image url" do
       send_message("tiqav ちくわ")
       expect(replies).to eq([ "http://img.tiqav.com/1aM.jpg" ])
+    end
+  end
+
+  describe "random" do
+    it "returns some image url" do
+      send_message("tiqav")
+      expect(replies[0]).to match(/^http:\/\/img\.tiqav\.com\//)
+      expect(replies.size).to eq(1)
     end
   end
 end
